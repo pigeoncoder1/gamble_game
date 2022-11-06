90ae87fed4db15e92af9ff60b78485a95a87dd3e
pigeoncoder1 <richyrayc@gmail.com>
2022-11-06 20:00:03 +0000

    Test Commit

--- gamble/gamble/urls.py
+++ gamble/gamble/urls.py
@@ -1,12 +1,13 @@
-from website.views import login,startGame,calculateWin,lobby,stats,joinGame,createdGame,doLogin,seeOutcome,checkHaveCoins
+from website.views import goToLogin,startGame,calculateWin,lobby,stats,joinGame,createdGame,doLogin,seeOutcome,checkHaveCoins,checkIsOwner,lookUpPlayer,createUser,goToCreateUser,hallOfFame,shop,addTokens
 from django.contrib import admin
 from django.urls import path
 wins = 0
 losses = 0
 #reminder to add all urls to views.py list
+#^lol
 
 urlpatterns = [
-    path('', login, name=""),
+    path('', goToLogin, name=""),
     path('doLogin', doLogin, name="doLogin"),
     path('lobby', lobby, name="lobby"),
     path('joinGame/<int:id>', joinGame, name='joinGame'),
@@ -16,4 +17,11 @@ urlpatterns = [
     path('createdGame', createdGame, name='createdGame'),
     path('seeOutcome/<int:id>', seeOutcome, name="seeOutcome"),
     path('checkHaveCoins', checkHaveCoins, name="checkHaveCoins"),
+    path('checkIsOwner/<int:id>', checkIsOwner, name="checkIsOwner"),
+    path('lookUpPlayer', lookUpPlayer, name="lookUpPlayer"),
+    path('createUser', createUser, name="createUser"),
+    path('goToCreateUser',goToCreateUser, name="goToCreateUser"),
+    path('hallOfFame', hallOfFame, name="hallOfFame"),
+    path('shop', shop, name='shop'),
+    path('addTokens',addTokens,name="addTokens")
 ]
